c09ed63e02e44a2bd48e41cde305c21df45c8049
MSSimpleBooking <info@moke-style.de>
2025-06-23 12:26:14 +0200

    Delete src directory

--- src/pages/Home.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home() {
-  return <div className="p-8">
-    <h1 className="text-3xl font-bold">Willkommen bei MS SimpleBooking!</h1>
-    <p className="mt-4">Melde dich an, buche ein Meeting und erhalte deine Rechnung.</p>
-  </div>;
-}
