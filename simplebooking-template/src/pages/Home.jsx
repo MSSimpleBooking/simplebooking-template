beb18b1a2204144de97ba58072359d3c1291dd29
MSSimpleBooking <info@moke-style.de>
2025-06-23 12:35:09 +0200

    Delete simplebooking-template directory

--- simplebooking-template/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Willkommen bei MS SimpleBooking!</h1>
-      <p className="mt-4">Melde dich an, buche ein Meeting und erhalte deine Rechnung.</p>
-    </div>
-  );
-}
