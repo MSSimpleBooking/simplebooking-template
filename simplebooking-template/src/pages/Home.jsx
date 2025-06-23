7f5a18727929ae3103b0b93631285a80470482e3
MSSimpleBooking <info@moke-style.de>
2025-06-23 12:21:35 +0200

    Update Home.jsx

--- simplebooking-template/src/pages/Home.jsx
+++ simplebooking-template/src/pages/Home.jsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Willkommen bei MS SimpleBooking!</h1>
-      <p className="mt-4">Melde dich an, buche ein Meeting und erhalte deine Rechnung.</p>
-    </div>
-  );
+  return <div>Willkommen bei MS SimpleBooking!</div>;
 }
